cc5fd92557263a625e3248dcf52517b6f022a6e1
noriko-terada <terada@virtual-tech.net>
2013-10-01 10:40:04 +0900

    タイムゾーンは+または-から始まる

--- src/main/java/jp/sourceforge/reflex/util/DateUtil.java
+++ src/main/java/jp/sourceforge/reflex/util/DateUtil.java
@@ -207,9 +207,16 @@ public class DateUtil {
 		String targetStr = null;
 		SimpleDateFormat format;
 
+		// タイムゾーンは+または-から始まる
 		int idz = dateStr.lastIndexOf("+");
+		int idx = dateStr.lastIndexOf(":");
+		if (idz == -1) {
+			if (idx > 0) {
+				idz = dateStr.lastIndexOf("-", idx);
+			}
+		}
 		if (idz > 0) {
-			int idx = dateStr.lastIndexOf(":");
+			//int idx = dateStr.lastIndexOf(":");
 			if (idx == -1 || idx + 1 >= dateStr.length()) {
 				throw new ParseException(
 						"The form at the date is not \"yyyy-MM-dd'T'HH:mm:ss+99:99\".",
@@ -219,7 +226,7 @@ public class DateUtil {
 			}
 			format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
 		} else {
-			targetStr = dateStr.replace(" ","T");
+			targetStr = dateStr.replace(" ", "T");
 			format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 		}
 
