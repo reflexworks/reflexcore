55cc00b535c67bfef293edba6ed738eecccd5e02
noriko-terada <terada@virtual-tech.net>
2013-11-01 09:37:01 +0900

    Deflater、Inflaterを使い回すよう修正。

--- src/main/java/jp/sourceforge/reflex/util/DeflateUtil.java
+++ src/main/java/jp/sourceforge/reflex/util/DeflateUtil.java
@@ -6,8 +6,135 @@ import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
 
+/**
+ * Deflate圧縮・解凍ユーティリティ.
+ * <p>
+ * バイト配列をDeflate圧縮・解凍します。<br>
+ * <br>
+ * Deflaterのメモリ管理について、zlibをJNIで呼び出しているようです。<br>
+ * DeflaterのnewのタイミングでJVMの管理外の領域に少なくとも辞書窓サイズ分のメモリを確保しているようです。
+ * (Deflateの辞書窓サイズはだいたい32KiBぐらいあるようです。)
+ * JVMの外のシステムのメモリを使用するため、使い終わったら必ずend()メソッドを呼び出してください。<br>
+ * スレッドセーフではないので、スレッドごとに本ユーティリティを生成して使用してください。<br>
+ * もしくはstaticメソッドを使用してください。
+ * </p>
+ */
 public class DeflateUtil {
+
+	/** デフォルト圧縮レベル */
+	public static final int DEFAULT_LEVEL = Deflater.BEST_SPEED;
+	/** デフォルト nowrap */
+	public static final boolean DEFAULT_NOWRAP = true;
 	
+	private static final int BUFFER = 1024;
+	
+	private int level;
+	private boolean nowrap;
+	private Deflater def;
+	private Inflater inf;
+	
+	/**
+	 * コンストラクタ
+	 * @param level 圧縮レベル (0 〜 9)
+	 * @param nowrap true の場合は GZIP 互換の圧縮を使用
+	 */
+	public DeflateUtil(int level, boolean nowrap) {
+		this.level = level;
+		this.nowrap = nowrap;
+	}
+	
+	/**
+	 * コンプレッサを閉じ、圧縮解除された入力をすべて破棄します。
+	 */
+	public void end() {
+		if (def != null) {
+			try {
+				def.end();
+			} catch (Exception e) {}	// Do nothing.
+		}
+		if (inf != null) {
+			try {
+				inf.end();
+			} catch (Exception e) {}	// Do nothing.
+		}
+	}
+	
+	/**
+	 * 圧縮.
+	 * <p>
+	 * Deflater生成、圧縮、JNIメモリ解放を行います。<br>
+	 * 圧縮レベル、nowrapについてはデフォルトの値を使用します。
+	 * </p>
+	 * @param dataByte 圧縮対象データ
+	 * @return deflate圧縮データ
+	 */
+	public static byte[] deflateOneTime(byte[] dataByte) 
+	throws IOException {
+		return deflateOneTime(dataByte, DEFAULT_LEVEL, DEFAULT_NOWRAP);
+	}
+	
+	/**
+	 * 圧縮.
+	 * <p>
+	 * Deflater生成、圧縮、JNIメモリ解放を行います。
+	 * </p>
+	 * @param dataByte 圧縮対象データ
+	 * @param level 圧縮レベル (0 〜 9)
+	 * @param nowrap true の場合は GZIP 互換の圧縮を使用
+	 * @return deflate圧縮データ
+	 */
+	public static byte[] deflateOneTime(byte[] dataByte, int level, boolean nowrap) 
+	throws IOException {
+		DeflateUtil deflateUtil = null;
+		try {
+			deflateUtil = new DeflateUtil(level, nowrap);
+			return deflateUtil.deflate(dataByte);
+			
+		} finally {
+			if (deflateUtil != null) {
+				deflateUtil.end();
+			}
+		}
+	}
+	
+	/**
+	 * 解凍.
+	 * <p>
+	 * Inflater生成、解凍、JNIメモリ解放を行います。<br>
+	 * nowrapについてはデフォルトの値を使用します。
+	 * </p>
+	 * @param dataByte deflate圧縮データ
+	 * @return 解凍データ
+	 */
+	public static byte[] inflateOneTime(byte[] dataByte) 
+	throws IOException, DataFormatException {
+		return inflateOneTime(dataByte, DEFAULT_NOWRAP);
+	}
+	
+	/**
+	 * 解凍.
+	 * <p>
+	 * Inflater生成、解凍、JNIメモリ解放を行います。<br>
+	 * </p>
+	 * @param dataByte deflate圧縮データ
+	 * @param nowrap true の場合は GZIP 互換の圧縮を使用
+	 * @return 解凍データ
+	 */
+	public static byte[] inflateOneTime(byte[] dataByte, boolean nowrap) 
+	throws IOException, DataFormatException {
+		DeflateUtil deflateUtil = null;
+		try {
+			deflateUtil = new DeflateUtil(DEFAULT_LEVEL, nowrap);
+			return deflateUtil.inflate(dataByte);
+			
+		} finally {
+			if (deflateUtil != null) {
+				deflateUtil.end();
+			}
+		}
+		
+	}
+
 	/**
 	 * 圧縮する
 	 * 
@@ -15,24 +142,40 @@ public class DeflateUtil {
 	 * @return deflated bytes
 	 * @throws IOException
 	 */
-	public byte[] deflate(byte[] dataByte) throws IOException {
-
-		Deflater def = new Deflater(Deflater.BEST_SPEED,true);	// GZIP 互換の圧縮をサポート(JSとの通信のため）
-//		def.setLevel(Deflater.BEST_SPEED);
-		def.setInput(dataByte);
-		def.finish();
-
+	public byte[] deflate(byte[] dataByte) 
+	throws IOException {
+		if (dataByte == null || dataByte.length == 0) {
+			return null;
+		}
+		
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(
 				dataByte.length);
-		byte[] buf = new byte[1024];
-		while (!def.finished()) {
-			int compByte = def.deflate(buf);
-			byteArrayOutputStream.write(buf, 0, compByte);
-		}
-		byteArrayOutputStream.close();
-		def.end(); 	// VM外メモリ解放　http://qiita.com/nishemon/items/031e476de08d623bfded
+		try {
+			if (def == null) {
+				def = new Deflater(level, nowrap);
+			}
+			
+			def.setInput(dataByte);
+			def.finish();
+	
+			byte[] buf = new byte[BUFFER];
+			while (!def.finished()) {
+				int compByte = def.deflate(buf);
+				byteArrayOutputStream.write(buf, 0, compByte);
+			}
 
-		return byteArrayOutputStream.toByteArray();
+			return byteArrayOutputStream.toByteArray();
+
+		} finally {
+			if (def != null) {
+				try {
+					def.reset();
+				} catch (Exception e) {}	// Do nothing.
+			}
+			try {
+				byteArrayOutputStream.close();
+			} catch (Exception e) {}	// Do nothing.
+		}
 	}
 
 	/**
@@ -43,22 +186,39 @@ public class DeflateUtil {
 	 * @throws IOException
 	 * @throws DataFormatException
 	 */
-	public byte[] inflate(byte[] dataByte) throws IOException,
-			DataFormatException {
-
-		Inflater inf = new Inflater(true);
-		inf.setInput(dataByte);
+	public byte[] inflate(byte[] dataByte) 
+	throws IOException, DataFormatException {
+		if (dataByte == null || dataByte.length == 0) {
+			return null;
+		}
 
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-		byte[] buf = new byte[1024];
-		while (!inf.finished()) {
-			int resultByte = inf.inflate(buf);
-			byteArrayOutputStream.write(buf, 0, resultByte);
-		}
-		byteArrayOutputStream.close();
-		inf.end(); 	// メモリ対策
 
-		return byteArrayOutputStream.toByteArray();
+		try {
+			if (inf == null) {
+				inf = new Inflater(nowrap);
+			}
+			
+			inf.setInput(dataByte);
+	
+			byte[] buf = new byte[BUFFER];
+			while (!inf.finished()) {
+				int resultByte = inf.inflate(buf);
+				byteArrayOutputStream.write(buf, 0, resultByte);
+			}
+			
+			return byteArrayOutputStream.toByteArray();
+
+		} finally {
+			if (inf != null) {
+				try {
+					inf.reset();
+				} catch (Exception e) {}	// Do nothing.
+			}
+			try {
+				byteArrayOutputStream.close();
+			} catch (Exception e) {}	// Do nothing.
+		}
 	}
 
 }
