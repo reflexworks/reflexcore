c14fa238d2c1c6124efa1de4d24844c5dd735bd0
noriko-terada <terada@virtual-tech.net>
2014-08-28 10:49:46 +0900

    不要なimportを削除

--- src/main/java/jp/sourceforge/reflex/util/DeflateUtil.java
+++ src/main/java/jp/sourceforge/reflex/util/DeflateUtil.java
@@ -5,7 +5,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
-import java.util.zip.DeflaterInputStream;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.Inflater;
 import java.util.zip.InflaterInputStream;
