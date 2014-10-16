25ff5f31fcbd8172723960b957c55700ac28b986
noriko-terada <terada@virtual-tech.net>
2014-10-16 14:04:55 +0900

    HeaderUtil#getHeaderValue 追加。

--- src/main/java/jp/reflexworks/servlet/util/HeaderUtil.java
+++ src/main/java/jp/reflexworks/servlet/util/HeaderUtil.java
@@ -37,6 +37,28 @@ public class HeaderUtil {
 	/**
 	 * ヘッダから指定されたキーの値を取得します.
 	 * <p>
+	 * 指定されたキーで取得できない場合、キーを小文字にして取得します。
+	 * </p>
+	 * @param headers ヘッダ
+	 * @param key キー
+	 * @return 値
+	 */
+	public static String getHeaderValue(Map<String, String> headers,
+			String key) {
+		if (headers == null || StringUtils.isBlank(key)) {
+			return null;
+		}
+		String val = headers.get(key);
+		if (val == null) {
+			val = headers.get(key.toLowerCase(Locale.ENGLISH));
+		}
+		return val;
+	}
+
+	/**
+	 * ヘッダから指定されたキーの値を取得します.
+	 * <p>
+	 * 指定されたキーで取得できない場合、キーを小文字にして取得します。<br>
 	 * 値がカンマ区切りで定義されている場合、内容をリストにして返却します。<br>
 	 * 値はトリミングして返却します。
 	 * </p>
@@ -46,13 +68,19 @@ public class HeaderUtil {
 	 */
 	public static Set<String> getHeaderValues(Map<String, String> headers,
 			String key) {
-		if (headers == null || StringUtils.isBlank(key)) {
-			return null;
-		}
-		String val = headers.get(key);
-		if (val == null) {
-			val = headers.get(key.toLowerCase(Locale.ENGLISH));
-		}
+		String val = getHeaderValue(headers, key);
+		return getHeaderValues(val);
+	}
+
+	/**
+	 * カンマ区切りのヘッダの内容をリストにして返却します.
+	 * <p>
+	 * 値はトリミングして返却します。
+	 * </p>
+	 * @param val 値
+	 * @return カンマ指定された値をリストにして返却
+	 */
+	public static Set<String> getHeaderValues(String val) {
 		if (val == null) {
 			return null;
 		}
@@ -63,7 +91,7 @@ public class HeaderUtil {
 		}
 		return values;
 	}
-	
+
 	/**
 	 * リストに指定された値をカンマ区切り文字列にします.
 	 * @param values リストに指定された値
@@ -103,7 +131,35 @@ public class HeaderUtil {
 			return false;
 		}
 		Set<String> values = getHeaderValues(headers, key);
-		if (values != null) {
+		return containsHeader(values, value);
+	}
+	
+	/**
+	 * ヘッダに値が設定されているかチェックします.
+	 * <p>
+	 * ヘッダの値がカンマ区切りになっている場合、カンマで区切られた値を一つづつチェックします。
+	 * </p>
+	 * @param headerValue ヘッダの値
+	 * @param value 値
+	 * @return ヘッダに値が設定されている場合true
+	 */
+	public static boolean containsHeader(String headerValue,
+			String value) {
+		if (StringUtils.isBlank(headerValue) || StringUtils.isBlank(value)) {
+			return false;
+		}
+		Set<String> values = getHeaderValues(headerValue);
+		return containsHeader(values, value);
+	}
+
+	/**
+	 * ヘッダの内容リストに指定されたキーの指定された値が設定されているかチェックします.
+	 * @param values ヘッダの内容リスト
+	 * @param value 値
+	 * @return リストに指定されたキーの指定された値が設定されている場合true
+	 */
+	public static boolean containsHeader(Set<String> values, String value) {
+		if (values != null && value != null) {
 			for (String tmpVal : values) {
 				if (value.equals(tmpVal)) {
 					return true;
