f1e11d99b24ea6bfbefc3431e027cba2ff54e038
takezaki <stakezaki@gmail.com>
2022-09-03 19:01:46 +0900

    Revert "Fix XMLSerializer"
    This reverts commit 9108dee435e174ff40e32c03c7dda8d2c267356e.

--- src/main/java/jp/sourceforge/reflex/core/XMLContext.java
+++ src/main/java/jp/sourceforge/reflex/core/XMLContext.java
@@ -9,13 +9,12 @@ import java.util.Map;
 import java.util.Stack;
 
 public class XMLContext {
-
 	
 	private Writer out;
 	private Stack<String> stack = new Stack<String>();
 	private Map<String,String> attrmap = new HashMap<String,String>();
 	public String nodename;
-	public String nodevalue;
+	private String nodevalue;
 	
 	public void setNode(Field field,Object source) throws IllegalArgumentException, IllegalAccessException {
 		this.nodename = getNodename(field.getName());
@@ -47,19 +46,8 @@ public class XMLContext {
 				  .replaceAll("'", "&apos;");
 	}
 	
-	
-	public void outNodename(String nodename, Object source) throws IOException, IllegalArgumentException, IllegalAccessException {
-		out.write("<"+getNodename(nodename));
-	    Field[] fields = source.getClass().getFields();
-
-	    for (int fn = 0; fn < fields.length; fn++) {
-	    	String attr = fields[fn].getName();
-	    	Object value = fields[fn].get(source);
-	    	if ((attr.indexOf("_$")==0)&&(attr.indexOf("_$$text")<0)&&value!=null) {
-	    		out.write(" "+attr.substring(2)+"=\""+value+"\"");
-	    	}
-	    }
-		out.write(">");
+	public void outNodename(String nodename) throws IOException {
+		out.write("<"+getNodename(nodename)+">");			
 	}
 
 	public void flush() throws IOException {
@@ -71,13 +59,11 @@ public class XMLContext {
 		out.write("<"+getNodename(nodename)+"/>");			
 	}
 
-
 	public void outNode() throws IOException {
 		if (this.nodevalue!=null&&this.nodename.indexOf("$xml")<0) {
-			if (this.nodename.equals("$$text")) {
+			if (this.nodename.equals("_$$text")) {
 				out.write(this.nodevalue);						
-			}else if (this.nodename.indexOf("$")<0)
-			{
+			}else {
 				out.write("<"+getNodename(this.nodename));
 				for(String key:attrmap.keySet()) {
 					out.write(" "+key+"=\""+convert(attrmap.get(key))+"\"");
@@ -87,7 +73,6 @@ public class XMLContext {
 				out.write("</"+getNodename(this.nodename)+">");									
 			}
 		}
-		this.nodevalue = null;
 	}
 
 	private String getNodename(String nodename) {
@@ -104,7 +89,7 @@ public class XMLContext {
 	}
 
 	public boolean addattr(Field field, Object source) throws IllegalArgumentException, IllegalAccessException, IOException {
-	  if (this.nodename!=null&&this.nodename.equals(field.getName())) {
+	  if (this.nodename.equals(field.getName())) {
 		  return true;
 	  }
   	  if (isSameNode(this.nodename,field.getName())) {
--- src/main/java/jp/sourceforge/reflex/core/XMLSerializer.java
+++ src/main/java/jp/sourceforge/reflex/core/XMLSerializer.java
@@ -12,66 +12,63 @@ import java.util.logging.Logger;
 public class XMLSerializer {
 
 	private Logger logger = Logger.getLogger(this.getClass().getName());
-	
+
 	public void marshal(Object source, Writer out) {
 		XMLContext context = new XMLContext(out);
 		marshal(context,source.getClass().getName(),source,false);
-//		marshal(context,source.getClass().getName(),null);
+		//		marshal(context,source.getClass().getName(),null);
 	}
 
 	public void marshal(XMLContext context,String nodename,Object source,boolean isList) {
 		try {
-		    if (source!=null&&source.getClass().getTypeName().equals("java.util.ArrayList")) {
-		        List list = (List) source;
-		          for (int i = 0; i < list.size(); i++) {
-		              this.marshal(context,null,list.get(i),true);
-		          }
-		    }else 
-			if (source==null) {
-				context.outNodenameOne(nodename);
-				context.flush();
-			}else {
-			    Field[] fields = source.getClass().getFields();
-			    if (nodename!=null&&!nodename.equals("")) {
-				    	if (nodename.startsWith("_")) nodename = nodename.substring(1);
-				        context.outNodename(nodename,source);   
-				        context.pushClosenode(nodename);
-			    }
+			if (source!=null&&source.getClass().getTypeName().equals("java.util.ArrayList")) {
+				List list = (List) source;
+				for (int i = 0; i < list.size(); i++) {
+					this.marshal(context,null,list.get(i),true);
+				}
+			}else 
+				if (source==null) {
+					context.outNodenameOne(nodename);
+					context.flush();
+				}else {
+					Field[] fields = source.getClass().getFields();
+					if (nodename!=null&&!nodename.equals("")) {
+						if (nodename.startsWith("_")) nodename = nodename.substring(1);
+						context.outNodename(nodename);   
+						context.pushClosenode(nodename);
+					}
 
-			    for (int fn = 0; fn < fields.length; fn++) {
+					for (int fn = 0; fn < fields.length; fn++) {
 
-			        if (fields[fn].get(source) == null)
-			          continue;
-			        if (isRealClass(fields[fn])) {
-			            Object child = fields[fn].get(source);
-			            if (child != null) {
-			              this.marshal(context, fields[fn].getName() ,child,false);
-			            }
-				      }else if (isTextNode(fields[fn])) {
-			    		  context.setNode(fields[fn],source);
-			    		  for (;(fn < fields.length)&&!isList; fn++) {
-			    				  if (!context.addattr(fields[fn],source)) {
-			    					  if ((fields.length>fn+1)&&!context.isSameNode(fields[fn].getName(),fields[fn+1].getName())) {
-			    						  context.nodevalue = null;
-				    					  break;			    						  
-			    					  }
-			    				  }
-					      }
-					      context.outNode();
-	    				  						      
+						if (fields[fn].get(source) == null)
+							continue;
+						if (isRealClass(fields[fn])) {
+							Object child = fields[fn].get(source);
+							if (child != null) {
+								this.marshal(context, fields[fn].getName() ,child,false);
+							}
+						}else if (isTextNode(fields[fn])) {
+							context.setNode(fields[fn],source);
+							for (;(fn < fields.length)&&!isList; fn++) {
+								if (!context.addattr(fields[fn],source)) {
+									if ((fields.length>fn+1)&&!context.isSameNode(fields[fn].getName(),fields[fn+1].getName())) {
+										context.outNode();
+										break;			    						  
+									}
+								}
+							}
+						}else if (isList(fields[fn])) {	
+							List list = (List) fields[fn].get(source);
+							for (int i = 0; i < list.size(); i++) {
+								this.marshal(context,fields[fn].getName(),list.get(i),true);
+							}
+						}
 
-			          }else if (isList(fields[fn])) {	
-			              List list = (List) fields[fn].get(source);
-			              for (int i = 0; i < list.size(); i++) {
-			                  this.marshal(context,fields[fn].getName(),list.get(i),true);
-			              }
-			          }
-			        
-			    }
-			    
-				context.popout();
-				
-			}
+					}
+
+					context.popout();
+
+				}
 		} catch (IOException e) {
 			logger.log(Level.WARNING, e.getClass().getName(), e);
 		} catch (IllegalArgumentException e) {
@@ -81,46 +78,46 @@ public class XMLSerializer {
 		}
 
 	}
-	
+
 	private boolean isRealClass(Field field) {
-		    if (Modifier.isFinal(field.getModifiers()))
-		      return false;
-		    
-		    String fieldname = field.getName();
-		    if (fieldname!=null) {
-		    	int i=0;
-		    	if (fieldname.startsWith("_")) i++;
-		      // 先頭に_が付く前提
-		      String classname = fieldname.substring(i, i+1).toUpperCase(Locale.ENGLISH) + fieldname.substring(i+1);
-		      if (field.getType().getName().indexOf(classname)>0) return true;
-		    }
-		    return false;
-		  }
-	  private boolean isList(Field field) {
-	    if (Modifier.isFinal(field.getModifiers()))
-	      return false;
-	    return (field.getType().getName().equals("java.util.List"));
-	  }
-	  
-	  private boolean isTextNode(Field field) {
-		  if (Modifier.isFinal(field.getModifiers()))
-			 return false;
-		  if (field.getType().getName().equals("java.lang.String")) {
-			  return true;			  
-		  }else
-		  if (field.getType().getName().equals("int") || field.getType().getName().equals("java.lang.Integer")) {
-			  return true;
-		  }else if (field.getType().getName().equals("long") || field.getType().getName().equals("java.lang.Long")) {
-			  return true;
-		  }else if (field.getType().getName().equals("float") || field.getType().getName().equals("java.lang.Float")) {
-			  return true;
-		  }else if (field.getType().getName().equals("double") || field.getType().getName().equals("java.lang.Double")) {
-			  return true;
-		  }else if (field.getType().getName().equals("boolean") || field.getType().getName().equals("java.lang.Boolean")) {
-			  return true;			  
-		  }else if (field.getType().getName().equals("java.util.Date")) {
-			  return true;
-		  }
-			  return false;		   
-	  }	  
+		if (Modifier.isFinal(field.getModifiers()))
+			return false;
+
+		String fieldname = field.getName();
+		if (fieldname!=null) {
+			int i=0;
+			if (fieldname.startsWith("_")) i++;
+			// 先頭に_が付く前提
+			String classname = fieldname.substring(i, i+1).toUpperCase(Locale.ENGLISH) + fieldname.substring(i+1);
+			if (field.getType().getName().indexOf(classname)>0) return true;
+		}
+		return false;
+	}
+	private boolean isList(Field field) {
+		if (Modifier.isFinal(field.getModifiers()))
+			return false;
+		return (field.getType().getName().equals("java.util.List"));
+	}
+
+	private boolean isTextNode(Field field) {
+		if (Modifier.isFinal(field.getModifiers()))
+			return false;
+		if (field.getType().getName().equals("java.lang.String")) {
+			return true;			  
+		}else
+			if (field.getType().getName().equals("int") || field.getType().getName().equals("java.lang.Integer")) {
+				return true;
+			}else if (field.getType().getName().equals("long") || field.getType().getName().equals("java.lang.Long")) {
+				return true;
+			}else if (field.getType().getName().equals("float") || field.getType().getName().equals("java.lang.Float")) {
+				return true;
+			}else if (field.getType().getName().equals("double") || field.getType().getName().equals("java.lang.Double")) {
+				return true;
+			}else if (field.getType().getName().equals("boolean") || field.getType().getName().equals("java.lang.Boolean")) {
+				return true;			  
+			}else if (field.getType().getName().equals("java.util.Date")) {
+				return true;
+			}
+		return false;		   
+	}	  
 }
